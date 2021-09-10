1808986ece4d096f8d5e32b2a6c06548d9172a3e
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-09-10 17:29:38 -0500

    initial commit

---
+++ MarvelousApi/MarvelousApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelousApi
+//
+//  Created by Ricardo Ramirez on 10/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
