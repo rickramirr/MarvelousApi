13ed502bc04329e589dccb23ce3e64fab4be3e8a
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-09-10 19:42:47 -0500

    new code for view model

--- MarvelousApi/MarvelousApi/MarvelousApi/ComicList/ViewModel/ComicListViewModel.swift
+++ MarvelousApi/MarvelousApi/MarvelousApi/ComicList/ViewModel/ComicListViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+class ComicListViewModel {
+    
+    let comicProvider: ComicProvider
+    
+    @Published var comics = [Comic]()
+    
+    @Published var isLoading = false
+    
+    var total: Int = 0
+    
+    init(comicProvider: ComicProvider = MarvelAPI()) {
+        self.comicProvider = comicProvider
+    }
+    
+    func getComicsIfNeeded() {
+        guard !isLoading else {
+            return
+        }
+        if comics.count == 0 || comics.count < total {
+            requestComics()
+        }
+    }
+    
+    func requestComics() {
+        isLoading = true
+        comicProvider.getComics(withOffset: comics.count) { response in
+            self.comics.append(contentsOf: response.data.results)
+            self.total = response.data.total
+            self.isLoading = false
+        }
+    }
+    
+}
