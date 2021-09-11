7a94be08bee6b47797620d12835a4a1091705afa
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-09-10 19:39:31 -0500

    new code for views

--- MarvelousApi/MarvelousApi/MarvelousApi/ComicList/View/ComicCell.swift
+++ MarvelousApi/MarvelousApi/MarvelousApi/ComicList/View/ComicCell.swift
@@ -5,4 +5,57 @@
 //  Created by Ricardo Ramirez on 10/09/21.
 //
 
-import Foundation
+import UIKit
+
+class ComicCell: UITableViewCell {
+    
+    static let cellIdentifier = String(describing: self)
+    
+    private lazy var comicImage: UIImageView = {
+        let iv = UIImageView()
+        return iv
+    }()
+    
+    private lazy var comicName: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        activateConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        contentView.addSubview(comicImage)
+        contentView.addSubview(comicName)
+    }
+    
+    func activateConstraints() {
+        comicImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comicImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            comicImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            comicImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            comicImage.widthAnchor.constraint(equalToConstant: 70),
+            comicImage.heightAnchor.constraint(equalToConstant: 140)
+        ])
+        
+        comicName.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comicName.centerYAnchor.constraint(equalTo: comicImage.centerYAnchor),
+            comicName.leadingAnchor.constraint(equalTo: comicImage.trailingAnchor, constant: 10),
+            comicName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+    
+    func updateUI(withComic comic: Comic) {
+        comicName.text = comic.title
+    }
+    
+}
--- MarvelousApi/MarvelousApi/MarvelousApi/ComicList/View/ComicListView.swift
+++ MarvelousApi/MarvelousApi/MarvelousApi/ComicList/View/ComicListView.swift
@@ -5,4 +5,78 @@
 //  Created by Ricardo Ramirez on 10/09/21.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+class ComicListView: UIViewController {
+    
+    var viewModel = ComicListViewModel()
+    
+    private enum ComicSection: CaseIterable {
+        case all
+    }
+    
+    private var dataSource: UITableViewDiffableDataSource<ComicSection,Comic>?
+    
+    private lazy var table: UITableView = {
+        let table = UITableView()
+        return table
+    }()
+    
+    override func loadView() {
+        super.loadView()
+        setupUI()
+        activateConstraints()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(table)
+    }
+    
+    private func activateConstraints() {
+        table.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            table.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            table.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            table.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            table.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func configureTable() {
+        let dataSource = makeDataSource()
+        self.dataSource = dataSource
+        table.dataSource = dataSource
+        table.delegate = self
+        table.register(ComicCell.self, forCellReuseIdentifier: ComicCell.cellIdentifier)
+    }
+    
+    func updateUI(withComics comics: [Comic]) {
+        var snapshot = NSDiffableDataSourceSnapshot<ComicSection,Comic>()
+        snapshot.appendSections([.all])
+        snapshot.appendItems(comics)
+        dataSource?.apply(snapshot)
+    }
+    
+}
+
+extension ComicListView: UITableViewDelegate {
+    
+}
+
+private extension ComicListView {
+    
+    private func makeDataSource() -> UITableViewDiffableDataSource<ComicSection,Comic> {
+        return UITableViewDiffableDataSource<ComicSection,Comic>(tableView: table) { (table, indexPath, comic) -> UITableViewCell? in
+            let cell = table.dequeueReusableCell(withIdentifier: ComicCell.cellIdentifier, for: indexPath) as? ComicCell
+            cell?.updateUI(withComic: comic)
+            return cell
+        }
+    }
+    
+}
