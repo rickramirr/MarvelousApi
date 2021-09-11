768d4bad5999e00ef18f9f4c742112ba74b4eada
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-09-10 19:39:51 -0500

    new model

--- MarvelousApi/MarvelousApi/MarvelousApi/ComicList/Model/Comic.swift
+++ MarvelousApi/MarvelousApi/MarvelousApi/ComicList/Model/Comic.swift
@@ -6,3 +6,77 @@
 //
 
 import Foundation
+
+struct Comic: Codable, Hashable {
+    let id: Int?
+    let digitalID: Int?
+    let title: String?
+    let issueNumber: Int?
+    let variantDescription: String?
+    let resultDescription: String?
+    let modified: String?
+    let isbn: String?
+    let upc :String?
+    let diamondCode: String?
+    let ean: String?
+    let issn: String?
+    let format: String?
+    let pageCount: Int?
+    let textObjects: [TextObject]?
+    let resourceURI: String?
+    let urls: [Url]?
+    let series: SeriesSummary?
+    let dates: [ComicDate]?
+    let prices: [ComicPrice]?
+    let thumbnail: Image?
+    let images: [Image]?
+    let creators: ItemList?
+    let characters: ItemList?
+    let stories: ItemList?
+    let events: ItemList?
+}
+
+struct ItemList: Codable, Hashable {
+    let available: Int?
+    let collectionURI: String?
+    let items: [Item]?
+    let returned: Int?
+}
+
+struct Item: Codable, Hashable {
+    let resourceURI: String?
+    let name: String?
+    let role: String?
+    let type: String?
+}
+
+struct ComicDate: Codable, Hashable {
+    let type: String?
+    let date: String?
+}
+
+struct Image: Codable, Hashable {
+    let path: String?
+    let thumbnailExtension: String?
+}
+
+struct ComicPrice: Codable, Hashable {
+    let type: String?
+    let price: Double?
+}
+
+struct SeriesSummary: Codable, Hashable {
+    let resourceURI: String?
+    let name: String?
+}
+
+struct TextObject: Codable, Hashable {
+    let type: String?
+    let language: String?
+    let text: String?
+}
+
+struct Url: Codable, Hashable {
+    let type: String?
+    let url: String?
+}
