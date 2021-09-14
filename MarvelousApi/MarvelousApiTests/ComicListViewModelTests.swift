bcec650d35c1162d6f99057c89f6d5ae2b2d03b8
Ricardo Ramirez <ricardo.ramirezr@elektra.com.mx>
2021-09-13 19:51:00 -0500

    changes to add tests

--- MarvelousApi/MarvelousApiTests/ComicListViewModelTests.swift
+++ MarvelousApi/MarvelousApiTests/ComicListViewModelTests.swift
@@ -1,14 +1,13 @@
 //
-//  MarvelousApiTests.swift
+//  ComicListViewModelTests.swift
 //  MarvelousApiTests
 //
-//  Created by Ricardo Ramirez on 10/09/21.
+//  Created by Ricardo Ramirez on 13/09/21.
 //
 
 import XCTest
-@testable import MarvelousApi
 
-class MarvelousApiTests: XCTestCase {
+class ComicListViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
